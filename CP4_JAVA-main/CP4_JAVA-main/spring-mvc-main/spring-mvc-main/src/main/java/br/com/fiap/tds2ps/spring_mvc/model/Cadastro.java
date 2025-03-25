23188735799c73ff61c42c69e39cf9d706e7d1a0
Renan Bezerra <123891426+renan-b-eth@users.noreply.github.com>
2025-03-24 22:20:54 -0300

    Update Cadastro.java

--- CP4_JAVA-main/CP4_JAVA-main/spring-mvc-main/spring-mvc-main/src/main/java/br/com/fiap/tds2ps/spring_mvc/model/Cadastro.java
+++ CP4_JAVA-main/CP4_JAVA-main/spring-mvc-main/spring-mvc-main/src/main/java/br/com/fiap/tds2ps/spring_mvc/model/Cadastro.java
@@ -1,12 +1,9 @@
 package br.com.fiap.tds2ps.spring_mvc.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-
+@Table(name = "CADASTRO")
 @Entity
 public class Cadastro {
 
